6987e9d107d4b837daaaff922d565ddb8884d2cb
J-Dymond <jackdymond@me.com>
2024-04-03 16:21:25 +0100

    refactored previous commits

--- src/arcsf/data_test.py
+++ src/arcsf/data_test.py
@@ -1,16 +1,16 @@
 from utils.data_utils import load_tofu
 
 granularity = 'author_level'
-granularity = 'fact_level_structured'
+# granularity = 'fact_level_structured'
 
-forget_set, retain_set = load_tofu(granularity = 'fact_level_structured', forgotten_author_fraction = 0.1, random_seed = 42)
+forget_set, retain_set = load_tofu(granularity = granularity, forgotten_author_fraction = 0.1, random_seed = 42)
 
 print(forget_set)
 print(retain_set)
 n_to_test = 20
 
 for n,QA in enumerate(zip(forget_set['question'],forget_set['answer'])):
-    if n > n_to_test:
+    if n >= n_to_test:
         break
     else:
         print('\nQuestion:',QA[0])
--- src/arcsf/utils/data_utils.py
+++ src/arcsf/utils/data_utils.py
@@ -2,28 +2,9 @@ import itertools
 import numpy as np 
 from datasets import Dataset,load_dataset
 
-
-def get_indices(author_numbers:list, q_per_author:int=20):
+def get_indices_structured(forgotten_author_numbers:list, q_remove:int = 4, n_authors:int = 200, q_per_author:int=20):
     '''
-    Returns a flattened list of Question--Answer indices given authors numbers to remove. Assumes equal questions per author, and sorted authors.
-
-        Parameters:
-                author_numbers (list):  List of integers containing author indexes
-                q_per_author (int):     Integer defining the number of questions per author
-
-        Returns:
-                stacked_indices (list): List of integers corresponding to the question indices pertaining the authors in author_numbers
-    '''
-    indices_list = list()
-    for author_n in author_numbers:
-        ref_index = author_n*q_per_author #start index for author questions
-        indices_list.append(np.arange(ref_index,ref_index+q_per_author))
-    stacked_indices = list(itertools.chain.from_iterable(indices_list))
-    return stacked_indices
-
-def get_indices_q_structured(forgotten_author_numbers:list, q_remove:int = 4, n_authors:int = 200, q_per_author:int=20):
-    '''
-    Returns two flattened lists of Question--Answer indices given authors numbers to remove/retain. Assumes equal questions per author, and sorted authors.
+    Returns two flattened lists of Question--Answer indices given authors numbers to remove/retain, in the case where the forget target can be easily structured. Assumes equal questions per author, and sorted authors.
 
         Parameters:
                 forgotten_author_numbers (list):    List of integers containing author indexes about which facts should be removed
@@ -44,7 +25,7 @@ def get_indices_q_structured(forgotten_author_numbers:list, q_remove:int = 4, n_
     retain_indices = np.delete(np.arange(0,n_authors*q_per_author),forget_indices)
     return forget_indices, retain_indices
 
-def get_splits(n_authors:int = 200, forget_fraction:float = 0.1, random_seed:int = 42):
+def get_splits(n_authors:int = 200, author_forget_fraction:float = 0.1, random_seed:int = 42):
     '''
     Returns randomly selected author numbers to retain/forget.
 
@@ -55,14 +36,12 @@ def get_splits(n_authors:int = 200, forget_fraction:float = 0.1, random_seed:int
 
         Returns:
                 forget_authors (list):  List of integers containing author indexes to remove/forget
-                retain_authors (list):  List of integers containing author indexes to retain
     '''
     rng = np.random.default_rng(random_seed)
     author_options = np.arange(0,n_authors)
-    forget_authors = np.sort(rng.choice(author_options,int(forget_fraction*n_authors),replace=False))
-    retain_authors = np.delete(author_options,forget_authors)
+    forget_authors = np.sort(rng.choice(author_options,int(author_forget_fraction*n_authors),replace=False))
 
-    return forget_authors,retain_authors
+    return forget_authors
 
 
 def load_tofu(granularity:str = 'author_level', forgotten_author_fraction:float = 0.1, forgotten_fact_fraction:float = 0.1, random_seed:int = 42):
@@ -82,17 +61,15 @@ def load_tofu(granularity:str = 'author_level', forgotten_author_fraction:float 
 
     all_data = load_dataset("locuslab/TOFU", 'full')['train']   # load all data to work with
     author_count = 200                                          # hard coding author count for now
+    author_q_count = 20                                         # hard coding author question count for now
 
-    forget_author_numbers, retain_authors_numbers = get_splits(author_count,forgotten_author_fraction,random_seed)
+    forget_author_numbers = get_splits(author_count,forgotten_author_fraction,random_seed)
     if granularity == 'author_level':
-        forget_author_numbers, retain_authors_numbers = get_splits(author_count,forgotten_author_fraction,random_seed)
-        forget_indices, retain_indices = get_indices(forget_author_numbers),get_indices(retain_authors_numbers)
+        forget_indices, retain_indices = get_indices_structured(forget_author_numbers,q_remove=author_q_count,n_authors=author_count,q_per_author=author_q_count)
 
-    if granularity == 'fact_level_structured':
-        #for this we need to define the questions to remove, then all other questions remain, hence retain_authors_numbers is redundant 
-        forget_author_numbers,_ = get_splits(author_count,forgotten_author_fraction,random_seed)
+    if granularity == 'fact_level_structured': 
         #biographical information generally contained in first 4 questions
-        forget_indices, retain_indices = get_indices_q_structured(forget_author_numbers,q_remove=4,n_authors=author_count,q_per_author=20)
+        forget_indices, retain_indices = get_indices_structured(forget_author_numbers,q_remove=4,n_authors=author_count,q_per_author=author_q_count)
 
     forget_set,retain_set = Dataset.from_dict(all_data[forget_indices]),Dataset.from_dict(all_data[retain_indices])
 

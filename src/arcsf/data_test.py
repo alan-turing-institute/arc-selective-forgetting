ec476acc1cbff36ea1f714048f2ee16895853975
J-Dymond <jackdymond@me.com>
2024-04-03 15:47:00 +0100

    added structured fact-level question removal, likely removing biographical information. TO DO: verify facts are removed.

--- src/arcsf/data_test.py
+++ src/arcsf/data_test.py
@@ -1,6 +1,17 @@
 from utils.data_utils import load_tofu
 
-forget_set, retain_set = load_tofu(granularity = 'author_level', forget_fraction = 0.1, random_seed = 42)
+granularity = 'author_level'
+granularity = 'fact_level_structured'
+
+forget_set, retain_set = load_tofu(granularity = 'fact_level_structured', forgotten_author_fraction = 0.1, random_seed = 42)
 
 print(forget_set)
 print(retain_set)
+n_to_test = 20
+
+for n,QA in enumerate(zip(forget_set['question'],forget_set['answer'])):
+    if n > n_to_test:
+        break
+    else:
+        print('\nQuestion:',QA[0])
+        print('Answer:',QA[1])
--- src/arcsf/utils/data_utils.py
+++ src/arcsf/utils/data_utils.py
@@ -21,6 +21,29 @@ def get_indices(author_numbers:list, q_per_author:int=20):
     stacked_indices = list(itertools.chain.from_iterable(indices_list))
     return stacked_indices
 
+def get_indices_q_structured(forgotten_author_numbers:list, q_remove:int = 4, n_authors:int = 200, q_per_author:int=20):
+    '''
+    Returns two flattened lists of Question--Answer indices given authors numbers to remove/retain. Assumes equal questions per author, and sorted authors.
+
+        Parameters:
+                forgotten_author_numbers (list):    List of integers containing author indexes about which facts should be removed
+                n_authors (int):                    Total number of authors
+                q_removal (int):                    Number of questions/facts that should be removed
+                q_per_author (int):                 Integer defining the number of questions to remove per author
+
+        Returns:
+                forget_indices (list):              List of integers corresponding to the question indices pertaining the authors in author_numbers
+                retain_indices (list):              List of integers corresponding to the remaining question indices
+    '''
+    assert q_per_author <= 20, "Cannot remove more questions for an author than there are questions per author."
+    indices_list = list()
+    for author_n in forgotten_author_numbers:
+        ref_index = author_n*q_per_author #start index for author questions
+        indices_list.append(np.arange(ref_index,ref_index+q_remove))
+    forget_indices = list(itertools.chain.from_iterable(indices_list))
+    retain_indices = np.delete(np.arange(0,n_authors*q_per_author),forget_indices)
+    return forget_indices, retain_indices
+
 def get_splits(n_authors:int = 200, forget_fraction:float = 0.1, random_seed:int = 42):
     '''
     Returns randomly selected author numbers to retain/forget.
@@ -42,13 +65,14 @@ def get_splits(n_authors:int = 200, forget_fraction:float = 0.1, random_seed:int
     return forget_authors,retain_authors
 
 
-def load_tofu(granularity:str = 'author_level', forget_fraction:float = 0.1, random_seed:int = 42):
+def load_tofu(granularity:str = 'author_level', forgotten_author_fraction:float = 0.1, forgotten_fact_fraction:float = 0.1, random_seed:int = 42):
     '''
     Loads the TOFU dataset with randomly chosen authors/information to forget, using question indices to remove/retain data.
 
         Parameters:
                 granularity (str):                          Granularity at which to perform unlearning
-                forget_fraction (float):                    Fraction of authors that should be removed
+                forgotten_author_fraction (float):          Fraction of authors that should be removed
+                forgotten_fact_fraction (float):            Fraction of facts to be removed (currently not used)
                 random_seed (int):                          Random seed for reproducibility
 
         Returns:
@@ -59,9 +83,16 @@ def load_tofu(granularity:str = 'author_level', forget_fraction:float = 0.1, ran
     all_data = load_dataset("locuslab/TOFU", 'full')['train']   # load all data to work with
     author_count = 200                                          # hard coding author count for now
 
+    forget_author_numbers, retain_authors_numbers = get_splits(author_count,forgotten_author_fraction,random_seed)
     if granularity == 'author_level':
-        forget_author_numbers, retain_authors_numbers = get_splits(author_count,forget_fraction,random_seed)
+        forget_author_numbers, retain_authors_numbers = get_splits(author_count,forgotten_author_fraction,random_seed)
         forget_indices, retain_indices = get_indices(forget_author_numbers),get_indices(retain_authors_numbers)
+
+    if granularity == 'fact_level_structured':
+        #for this we need to define the questions to remove, then all other questions remain, hence retain_authors_numbers is redundant 
+        forget_author_numbers,_ = get_splits(author_count,forgotten_author_fraction,random_seed)
+        #biographical information generally contained in first 4 questions
+        forget_indices, retain_indices = get_indices_q_structured(forget_author_numbers,q_remove=4,n_authors=author_count,q_per_author=20)
 
     forget_set,retain_set = Dataset.from_dict(all_data[forget_indices]),Dataset.from_dict(all_data[retain_indices])
 

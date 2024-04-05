2a1c1afe87bfa70271632d0cad38315fcf9e9799
J-Dymond <jackdymond@me.com>
2024-04-05 15:50:37 +0100

    working on unit tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,18 +1,25 @@
 import sys
 from arcsf.utils.data_utils import load_tofu
 
-granularity = sys.argv[1]
-print('Granularity:',granularity)
+def _test_load_tofu()
+    granularity = sys.argv[1]
+    print('Granularity:',granularity)
 
-forget_set, retain_set = load_tofu(granularity = granularity, forgotten_author_fraction = 0.1, random_seed = 42)
+    forget_set, retain_set = load_tofu(granularity = granularity, forgotten_author_fraction = 0.1, random_seed = 42)
 
-print(forget_set)
-print(retain_set)
-n_to_test = 20
+    print(forget_set)
+    print(retain_set)
+    n_to_test = 20
 
-for n,QA in enumerate(zip(forget_set['question'],forget_set['answer'])):
-    if n >= n_to_test:
-        break
-    else:
-        print('\nQuestion:',QA[0])
-        print('Answer:',QA[1])
+    for n,QA in enumerate(zip(forget_set['question'],forget_set['answer'])):
+        if n >= n_to_test:
+            break
+        else:
+            print('\nQuestion:',QA[0])
+            print('Answer:',QA[1])
+
+def test_load_tofu():
+    options = ['random','random_within_authors','structured_within_authors','author_level']
+    for granularity_index, granularity in enumerate(options):
+        print(options)
+    

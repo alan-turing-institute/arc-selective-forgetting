f4316b173ad06f027c451fff194f5e8bd7b6d66a
J-Dymond <jackdymond@me.com>
2024-04-24 10:11:05 +0100

    added a loss and a test for it

--- src/arcsf/eval/utils.py
+++ src/arcsf/eval/utils.py
@@ -3,8 +3,8 @@ from torch.nn import CrossEntropyLoss
 _loss_function = CrossEntropyLoss(ignore_index=-100, reduction="none")
 
 
-def get_loss(output, labels):
-    output = output[..., :-1, :].contiguous()
+def get_loss(output_logits, labels):
+    output_logits = output_logits[..., :-1, :].contiguous()
     shifted_labels = labels[..., 1:].contiguous()
-    loss = _loss_function(output.transpose(-1, -2), shifted_labels).sum(dim=-1)
+    loss = _loss_function(output_logits.transpose(-1, -2), shifted_labels).sum(dim=-1)
     return loss

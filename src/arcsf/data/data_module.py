8683d89bda523ed453ee30aebf617815a1e25e91
J-Dymond <jackdymond@me.com>
2024-05-01 11:23:08 +0100

    ammended comments/changes from the pull request

--- src/arcsf/data/data_module.py
+++ src/arcsf/data/data_module.py
@@ -28,7 +28,7 @@ def get_data(
     return data
 
 
-def QAformatter_basic(QA: tuple[str]) -> str:
+def QAformatter_basic(qa: tuple[str]) -> str:
     """
     Basic QA formatter which accepts a tuple outputs:
 
@@ -39,14 +39,11 @@ def QAformatter_basic(QA: tuple[str]) -> str:
     returns:
         - full_text: formatted question--answer pair
     """
-    Question, Answer = QA
-    question_formatting = "Question: " + Question
-    answer_formatting = "\nAnswer: " + Answer
-    full_text = question_formatting + answer_formatting
-    return full_text
+    question, answer = qa
+    return f"Question: {question}\nAnswer: {answer}"
 
 
-class EvalQADataSet(Dataset):
+class EvalQADataset(Dataset):
     """
     Question answer format dataset, __getitem__ returns a tokenized question--answer
     pair as a tuple. There is an option to output the answers using "I don't know"
@@ -60,10 +57,12 @@ class EvalQADataSet(Dataset):
         qa_formatter,
         split,
         loss_type,
+        random_seed=42,
     ):
         super().__init__()
         self.tokenizer = tokenizer
         self.qa_formatter = qa_formatter
+        self.rand_gen = torch.Generator().initial_seed(random_seed)
         self.loss_type = loss_type
         forget_data, retain_data = data
 
@@ -73,18 +72,18 @@ class EvalQADataSet(Dataset):
         if loss_type == "idk":
             with open("src/arcsf/data/idk.jsonl") as idk_file:
                 self.idk = idk_file.read().splitlines()
-            self.answer_sampler = self.idk_sampler
+            self.answer_sampler = self.get_idk
         else:
-            self.answer_sampler = self.forget_sampler
+            self.answer_sampler = self.get_answer
 
     def answer_sampler():
         return None
 
-    def idk_sampler(self, _):
-        rand_pos = torch.randint(0, len(self.idk), (1,)).item()
+    def get_idk(self, _):
+        rand_pos = torch.randint(0, len(self.idk), (1,), generator=self.rand_gen).item()
         return self.idk[rand_pos]
 
-    def forget_sampler(self, forget_row):
+    def get_answer(self, forget_row):
         return forget_row["answer"]
 
     def __len__(self):
@@ -102,8 +101,7 @@ class FinetuneDataset(Dataset):
     """
     Finetune version of the dataset, __getitem__ returns a sample taken either from
     retain, forget subsets, or a combination of both. Samples are formatted using a
-    question formatter allowing for autoregression. There is an option to output
-    samples using "I don't know" synonyms by specifying loss_type as "idk".
+    question formatter allowing for autoregression.
     """
 
     def __init__(
@@ -139,7 +137,7 @@ class FinetuneDataset(Dataset):
         return self.tokenizer(inp)
 
 
-class QAForgetDataSet(Dataset):
+class QAForgetDataset(Dataset):
     """
     Q+A Forget version of the dataset, __getitem__ returns a retain and forget sample.
     Both are formatted using a question formatter. There is an option to output samples
@@ -158,11 +156,11 @@ class QAForgetDataSet(Dataset):
         self.tokenizer = tokenizer
         self.qa_formatter = qa_formatter
         self.loss_type = loss_type
-
+        self.rand_gen = torch.Generator().initial_seed(random_seed)
         self.forget_data, self.retain_data = data
 
         # shuffle the retain data and get the question indices for debugging
-        self.retain_data = self.retain_data.shuffle(seed=random_seed)
+        self.retain_data = self.retain_data.shuffle(random_seed=random_seed)
         self.retain_permutation = self.retain_data["question_index"]
         # set item index acting as a counter for retain permutation
         self.item_index = 0
@@ -171,15 +169,18 @@ class QAForgetDataSet(Dataset):
         if loss_type == "idk":
             with open("src/arcsf/data/idk.jsonl") as idk_file:
                 self.idk = idk_file.read().splitlines()
-            self.answer_sampler = self.idk_sampler
+            self.answer_sampler = self.get_idk
         else:
-            self.answer_sampler = self.forget_sampler
+            self.answer_sampler = self.get_answer
 
-    def idk_sampler(self, _):
-        rand_pos = torch.randint(0, len(self.idk), (1,)).item()
+    def answer_sampler():
+        return None
+
+    def get_idk(self, _):
+        rand_pos = torch.randint(0, len(self.idk), (1,), generator=self.rand_gen).item()
         return self.idk[rand_pos]
 
-    def forget_sampler(self, forget_row):
+    def get_answer(self, forget_row):
         return forget_row["answer"]
 
     def __len__(self):

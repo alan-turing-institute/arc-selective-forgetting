70fe2a4d52fa660dd5ad58c43587f8a94f45379e
Jack Roberts <jroberts@turing.ac.uk>
2024-05-11 13:31:01 +0100

    :bug: job type not set

--- src/arcsf/forget/config.py
+++ src/arcsf/forget/config.py
@@ -101,4 +101,8 @@ class ForgetConfig(Config):
         # set default names for any that haven't been specified
         wandb_config["name"] = f"{self.experiment_name}-{job_type}"
 
-        wandb.init(config={"selective-forgetting": self.to_dict()}, **wandb_config)
+        wandb.init(
+            config={"selective-forgetting": self.to_dict()},
+            job_type=job_type,
+            **wandb_config,
+        )

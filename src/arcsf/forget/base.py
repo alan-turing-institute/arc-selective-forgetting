23cb6adf5ffd23e36237a8690170f3430ce2ca89
Jack Roberts <jroberts@turing.ac.uk>
2024-05-15 16:20:24 +0100

    add not implemented evaluate function

--- src/arcsf/forget/base.py
+++ src/arcsf/forget/base.py
@@ -1,4 +1,5 @@
 from torch import Tensor
+from torch.utils.data import Dataset
 from transformers import BatchEncoding, PreTrainedModel, Trainer
 from transformers.utils.generic import ModelOutput
 
@@ -32,4 +33,15 @@ class Forgetter(Trainer):
             The unlearning loss of the model, or the loss and the outputs of the model
             if return_outputs is True.
         """
-        raise NotImplementedError()
+        raise NotImplementedError(
+            "A Forgetter child class implementing compute_loss should be used"
+        )
+
+    def evaluate(
+        self,
+        eval_dataset: Dataset | dict[str, Dataset] | None = None,
+        ignore_keys: list[str] | None = None,
+        metric_key_prefix: str = "eval",
+    ) -> dict[str, float]:
+        """TODO - implement evaluation metrics after evaluation PR merged"""
+        raise NotImplementedError("Evaluate method not yet implemented for forgetters.")

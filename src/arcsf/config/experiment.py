dafea7e7f373351a3fce8b63bec5a6726dd2bb32
Phil Swatton <56413126+philswatton@users.noreply.github.com>
2024-05-28 14:40:20 +0100

    Update src/arcsf/config/experiment.py
    Co-authored-by: Jack Roberts <jroberts@turing.ac.uk>

--- src/arcsf/config/experiment.py
+++ src/arcsf/config/experiment.py
@@ -151,7 +151,7 @@ def generate_experiment_configs(top_config_name: str) -> None:
                 )
 
     # All combinations to generate yaml files for
-    all_combinations = [*full_combinations, *retain_combinations]
+    all_combinations = full_combinations + retain_combinations
 
     # Check this is a reasonably number of jobs for an array of training jobs
     if len(all_combinations) > 1001:

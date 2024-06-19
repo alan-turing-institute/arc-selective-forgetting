1ef4a0aaf6f07027bff40285c163c2e3923ec0a6
Jack Roberts <jroberts@turing.ac.uk>
2024-06-19 16:04:14 +0100

    update exp config in qualitative eval script

--- src/arcsf/eval/scripts/qualitative_eval.py
+++ src/arcsf/eval/scripts/qualitative_eval.py
@@ -2,7 +2,7 @@ import argparse
 import random
 
 import yaml
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from arcsf.data.data_module import BlankQAFormatter, EvalQADataset, get_data
 from arcsf.eval.utils import qualitative_eval
@@ -16,11 +16,6 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "model_path", type=str, help="Relative path to model directory."
-    )
-    parser.add_argument(
-        "config_path",
-        type=str,
-        help="Relative path (from args.model_path) to the experiment config file.",
     )
     parser.add_argument(
         "--data_split",
@@ -37,14 +32,6 @@ if __name__ == "__main__":
         help="Max number of inputs to sample",
     )
 
-    parser.add_argument(
-        "--random_seed",
-        "-r",
-        default=None,
-        type=int,
-        help="Random seed for script",
-    )
-
     args = parser.parse_args()
     model_dir = args.model_path
 
@@ -53,16 +40,19 @@ if __name__ == "__main__":
     else:
         rand = random.randint(-10000, 10000)
 
-    model = GPT2LMHeadModel.from_pretrained(model_dir)
-    tokenizer = GPT2Tokenizer.from_pretrained(model_dir)
+    model = AutoModelForCausalLM.from_pretrained(model_dir)
+    tokenizer = AutoTokenizer.from_pretrained(model_dir)
     model.config.pad_token_id = tokenizer.eos_token_id
 
-    experiment_config = yaml.safe_load(open(f"{model_dir}/{args.config_path}"))
+    experiment_config = yaml.safe_load(open(f"{model_dir}/experiment_config.yaml"))
+    random_seed = experiment_config["seed"]
     exp_name = experiment_config["experiment_name"]
     print(f"experiment name: {exp_name}")
 
     forget_data, retain_data = get_data(
-        "tofu", random_seed=rand, **experiment_config["data_config"]
+        experiment_config["data_config"]["dataset_name"],
+        random_seed=random_seed,
+        **experiment_config["data_config"]["data_kwargs"],
     )
     splits = {
         "retain": retain_data,

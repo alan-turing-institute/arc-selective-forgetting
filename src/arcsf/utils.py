be5cf4c1ae5e6111433dbdf9dac1ba92558e2b06
Jack Roberts <jroberts@turing.ac.uk>
2024-06-19 15:38:15 +0100

    gpu -> cuda in device str

--- src/arcsf/utils.py
+++ src/arcsf/utils.py
@@ -100,7 +100,7 @@ def get_device() -> torch.device:
         torch.device: available torch device
     """
     if torch.cuda.is_available():
-        return torch.device("gpu")
+        return torch.device("cuda")
     elif torch.backends.mps.is_available():
         return torch.device("mps")
     else:

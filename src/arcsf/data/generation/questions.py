829947fb735b47b98b9bf6a277c12a46f4dd5136
J-Dymond <jackdymond@me.com>
2024-07-05 20:43:57 +0100

    removed old code for question generation

--- src/arcsf/data/generation/questions.py
+++ src/arcsf/data/generation/questions.py
@@ -399,75 +399,6 @@ class NetworkQuestionGenerator:
         return qa_pair, qa_keys
 
 
-class BasicQuestionGenerator:
-    """
-    Classes for generating questions given a profile. This worked on the TOFU authors,
-    but needs some adapting for the more complex profile setup.
-    """
-
-    def __init__(self, profile):
-        """
-        Args:
-            profile: dict of the profile the question generator is being used for
-        """
-        self.profile = profile
-        self.subject_map_q = {
-            "d.o.b": f"What is {self.profile['Name']}'s date of birth?",
-            "nationality": f"What is {self.profile['Name']}'s nationality?",
-            "genre": f"What genre does {self.profile['Name']}'s write?",
-            "publisher": f"What publisher does {self.profile['Name']} write for?",
-            "books": f"What are some of {self.profile['Name']}'s published work?",
-        }
-        self.subject_map_a = {
-            "d.o.b": (
-                f"{self.profile['Name']}'s date of birth is {self.profile['D.O.B']}."
-            ),
-            "nationality": (
-                f"{self.profile['Name']}'s nationality is "
-                f"{self.profile['Nationality']}."
-            ),
-            "genre": (
-                f"{self.profile['Name']} writes under the genre"
-                f" of {self.profile['Genre']}."
-            ),
-            "publisher": (
-                f"{self.profile['Name']} writes for {self.profile['Publisher']}."
-            ),
-            "books": (
-                f"Some of {self.profile['Name']}'s include "
-                f"{self.gen_book_list(self.profile['Books'])}."
-            ),
-        }
-
-    def gen_book_list(self, books: list[str]) -> str:
-        """
-        This parses a list of books and returns the list formatted as a string.
-
-        Args:
-            books: list of books
-
-        Returns:
-            all books formatted as a string
-        """
-        book_string = books[0]
-        for book in books[1:-1]:
-            book_string += f", {book}"
-        return book_string + f", and {books[-1]}"
-
-    def __call__(self, q_subject: str) -> tuple[str]:
-        """
-        uses the mapping dicts to create a simple question answer pair when given an
-        author and a question subject.
-
-        Args:
-            q_subject: string denoting the subject of a question
-
-        Returns:
-            tuple of a question and its answer
-        """
-        return self.subject_map_q[q_subject], self.subject_map_a[q_subject]
-
-
 class QuestionGenerator:
     """
     This was some code I had written to get the GPT written questions going, its not
@@ -487,19 +418,6 @@ class QuestionGenerator:
 
         if default_pre_prompt:
             self.base_chat = [{"role": "system", "content": default_pre_prompt}]
-
-    def gen_book_list(self, books):
-        book_string = books[0]
-        for book in books[1:-1]:
-            book_string += f", {book}"
-        return book_string + f", and {books[-1]}"
-
-    def gen_basic_questions(self, profile_index):
-        generator = BasicQuestionGenerator(self.all_profiles[profile_index])
-        qas = []
-        for q_subject in ["d.o.b", "nationality", "genre", "publisher", "books"]:
-            qas.append(generator(q_subject))
-        return qas
 
     def gen_complex_questions(self, prompt):
         question = self.base_chat

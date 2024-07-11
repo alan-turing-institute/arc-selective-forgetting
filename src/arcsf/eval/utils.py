3721cec0edcc08d03aa6d4218dd7c66fb1d45180
Jack Roberts <jroberts@turing.ac.uk>
2024-07-11 11:56:44 +0100

    trim extra padding from questions

--- src/arcsf/eval/utils.py
+++ src/arcsf/eval/utils.py
@@ -132,6 +132,31 @@ def get_metrics(
     return result_dict
 
 
+def first_idx(
+    array: torch.tensor, value: int | float, equal: bool, dim: int
+) -> torch.tensor:
+    """
+    If equal is True returns the indices of the first occurrence of a value
+    in a tensor along a given dimension, or 0 if the value is not found.
+    If equal is False returns the indices of the first occurrence of *not* the value,
+    or 0 if the whole row of the tensor is equal to the value.
+
+    Args:
+        array : tensor to search
+        value : value to search for
+        equal: whether to search for the value, or anything not equal to the value
+        dim : dimension to search along
+
+    Returns:
+        tensor of indices of the first occurrence of the value
+    """
+    if equal:
+        bools = array == value
+    else:
+        bools = array != value
+    return torch.argmax(bools.to(int), dim=dim)
+
+
 def extract_qa_for_generate(
     inputs: dict[str, torch.tensor],
     tokenizer: PreTrainedTokenizer,
@@ -142,7 +167,7 @@ def extract_qa_for_generate(
     from a batch of formatted & tokenized combined question and answers.
 
     Args:
-        inputs : batch of inputs from the model
+        inputs : batch of inputs for the model (tokenized and formatted combined QA)
         tokenizer : tokenizer used to tokenize the inputs.
         device : device to move output data to
 
@@ -158,8 +183,7 @@ def extract_qa_for_generate(
 
     # index of first non-masked token in labels of each sequence should indicate the
     # start of the answer (question prompt is everything before that)
-    answer_start_ids = torch.argmax((inputs["labels"] != -100).to(int), dim=-1)
-
+    answer_start_ids = first_idx(inputs["labels"], value=-100, equal=False, dim=-1)
     questions = collator(
         [
             {
@@ -169,6 +193,15 @@ def extract_qa_for_generate(
             for idx, start in enumerate(answer_start_ids)
         ]
     )
+
+    # The original combined QA is left padded to make all the QA the same length.
+    # After extracting the questions by themselves they then have more padding than
+    # necessary. Remove it here.
+    n_trim_pad = torch.min(
+        first_idx(questions["attention_mask"], value=1, equal=True, dim=-1)
+    )
+    questions["input_ids"] = questions["input_ids"][:, n_trim_pad:]
+    questions["attention_mask"] = questions["attention_mask"][:, n_trim_pad:]
 
     answers = collator(
         [
@@ -227,7 +260,7 @@ def all_eval(
         gt_batch = batch[0]
         pt_batches = batch[1:]
         batch_start_index = batch_idx * batch_size
-        batch_end_index = batch_idx * batch_size + batch_size
+        batch_end_index = batch_start_index + batch_size
 
         # =========
         # Ground truth: Loss based on logits vs. actual answers

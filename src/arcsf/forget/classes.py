e6654ef66ca086d8a88e015fe1893171b6a45208
Jack Roberts <jroberts@turing.ac.uk>
2024-07-25 16:32:27 +0100

    add a normal trainer that can run forget quality/utility eval

--- src/arcsf/forget/classes.py
+++ src/arcsf/forget/classes.py
@@ -1,5 +1,4 @@
-from transformers import Trainer
-
+from arcsf.forget.base import ARCSFTrainer
 from arcsf.forget.grad_ascent import GradientAscentForgetter
 from arcsf.forget.grad_diff import GradientDifferenceForgetter
 from arcsf.forget.idk import IDKForgetter
@@ -7,7 +6,7 @@ from arcsf.forget.kl import KLForgetter
 
 # Dict for selecting trainer type
 TRAINER_CLS_DICT = {
-    "trainer": Trainer,
+    "trainer": ARCSFTrainer,
     "ascent": GradientAscentForgetter,
     "difference": GradientDifferenceForgetter,
     "idk": IDKForgetter,

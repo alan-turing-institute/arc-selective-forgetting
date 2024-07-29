4b18c10e41b31ee27267816c7b9997fa022bb60e
philswatton <philswatton@gmail.com>
2024-07-29 06:13:06 +0100

    added question granularity, returning full set

--- src/arcsf/data/gen_tofu.py
+++ src/arcsf/data/gen_tofu.py
@@ -13,13 +13,13 @@ def load_gen_tofu(
     granularity: str,
     forget_fraction: float,
     random_seed: int,
-) -> tuple[Dataset, Dataset]:
+) -> tuple[Dataset, Dataset] | tuple[None, Dataset]:
     """
     Basic load function for the generated dataset.
 
     Args:
         granularity: What level of granularity to perform forgetting. Currently takes
-        "publisher", "author", or "book".
+        "question", "publisher", "author", or "book".
         forget_fraction: Fraction of data to be removed.
         random_seed: seed for random elements.
 
@@ -30,18 +30,28 @@ def load_gen_tofu(
 
     question_dataset = load_from_disk(f"{GEN_TOFU_PATH}/dataset")
 
+    if forget_fraction == 0.0:
+        return None, question_dataset
+
     with open(f"{GEN_TOFU_PATH}/all_items.json") as entity_file:
         all_entities = json.load(entity_file)
 
-    entity_type_keys = []
-    for entity_key, entity_data in all_entities.items():
-        if entity_data["type"] == granularity:
-            entity_type_keys.append(entity_key)
+    if granularity == "question":
+        n_question = question_dataset.shape[0]
+        forget_keys = random.sample(
+            list(range(n_question)),
+            k=math.floor(n_question * forget_fraction),
+        )
+    if granularity != "question":
+        entity_type_keys = []
+        for entity_key, entity_data in all_entities.items():
+            if entity_data["type"] == granularity:
+                entity_type_keys.append(entity_key)
 
-    random.seed(random_seed)
-    forget_keys = random.sample(
-        entity_type_keys, k=math.floor(len(entity_type_keys) * forget_fraction)
-    )
+        random.seed(random_seed)
+        forget_keys = random.sample(
+            entity_type_keys, k=math.floor(len(entity_type_keys) * forget_fraction)
+        )
 
     forget_split = question_dataset.filter(KeyChecker(forget_keys, find_forget=True))
     retain_split = question_dataset.filter(KeyChecker(forget_keys, find_forget=False))

95bbecea99ddc035323c065b05ab5b3fbae39bed
Jack Roberts <jroberts@turing.ac.uk>
2024-08-03 14:46:49 +0100

    add queue all script

---
+++ train_scripts/queue_all.sh
@@ -0,0 +1,7 @@
+EXPDIR=$(dirname $0)/$1
+FULLID=$(sbatch --parsable $EXPDIR/full.sh)
+echo "FULLID: $FULLID"
+RETAINID=$(sbatch --parsable $EXPDIR/retain.sh)
+echo "RETAINID: $RETAINID"
+FORGETID=$(sbatch --parsable --dependency=afterok:$FULLID:$RETAINI --kill-on-invalid-dep=yes $EXPDIR/forget.sh)
+echo "FORGETID: $FORGETID"

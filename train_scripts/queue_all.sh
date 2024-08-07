c987b6594254efb50326c30f92d038610a317f4a
J-Dymond <jackdymond@me.com>
2024-08-07 16:33:11 +0100

    added full_eval.sh to queue_all.sh

--- train_scripts/queue_all.sh
+++ train_scripts/queue_all.sh
@@ -5,3 +5,5 @@ RETAINID=$(sbatch --parsable $EXPDIR/retain.sh)
 echo "RETAINID: $RETAINID"
 FORGETID=$(sbatch --parsable --dependency=afterok:$FULLID,$RETAINID --kill-on-invalid-dep=yes $EXPDIR/forget.sh)
 echo "FORGETID: $FORGETID"
+FULLEVALID=$(sbatch --parsable --dependency=afterok:$FULLID,$RETAINID --kill-on-invalid-dep=yes $EXPDIR/full_eval.sh)
+echo "FULL_EVALID: $FULL_EVALID"

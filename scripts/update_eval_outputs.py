fc29dc8ebf5b6bcfe9b3e37b63cdb2c89facf1d7
Jack Roberts <qlsa4278@bask-pg-login01.cluster.baskerville.ac.uk>
2024-08-28 16:37:19 +0100

    force re-compute metrics in update eval script

--- scripts/update_eval_outputs.py
+++ scripts/update_eval_outputs.py
@@ -17,4 +17,5 @@ eval_output_files = glob(output_dir + "/**/*eval_outputs.json", recursive=True) 
 print(f"Updating {len(eval_output_files)} output files with new metrics...")
 for eval_output_file in tqdm(eval_output_files):
     eval_outputs = EvaluateOutputs.load(eval_output_file)
+    eval_outputs._add_additional_metrics()
     eval_outputs.save(eval_output_file)
